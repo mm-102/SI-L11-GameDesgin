897e08712598ad1931cad34f26092ad61d6fbfc1
mm-102 <maciej.kowalski@student.put.poznan.pl>
2024-12-08 21:48:26 +0100

    Use tkinter

--- src/main.py
+++ src/main.py
@@ -2,10 +2,19 @@ import clips
 import os
 import tkinter as tk
 
-# widget = tk.Tk()
-
-
 CONSTRUCT = os.path.dirname(__file__)+"/../constructs/"
+
+widget = tk.Tk()
+widget.geometry("400x600")
+
+
+question_var = tk.StringVar(value="Question")
+question_label = tk.Label(widget, textvariable=question_var, font=('calibre',10, 'bold'))
+question_label.pack()
+
+answer_var = tk.StringVar(widget)
+answer_buttons_frame = tk.Frame(widget)
+answer_buttons_frame.pack()
 
 env = clips.Environment()
 env.clear()
@@ -14,35 +23,38 @@ env.load(CONSTRUCT+"facts.clp")
 env.load(CONSTRUCT+"rules.clp")
 env.reset()
 
-def answer_question(question_fact: dict[str, str | tuple]) -> None:
-    options = (question_fact[key] for key in question_fact if key.startswith("answer") and question_fact[key])
-    answers = {option[0]:option[1] for option in options}
-
-    while True:
-        print(question_fact["display"])
-        print('\n'.join(answers.keys()))
-        ans = input()
-        if ans in answers:
-            env.assert_string(f"(answer {answers[ans]})")
-            return
-
-        
-
-decision_made = False
-while not decision_made:
+def next_action() -> None:
     env.run()
     for fact in env.facts():
-        # print(fact)
         template_name = fact.template.name
 
         if template_name == "decision":
-            decision_made = True
-            print(fact)
+            question_var.set(tuple(fact))
             break
 
         if template_name == "question":
-            answer_question(dict(fact))
+            request_answer(dict(fact))
             fact.retract()
+            break
 
+def answer_question(value: str) -> None:
+    global answer_buttons_frame
+    env.assert_string(f"(answer {value})")
+    answer_buttons_frame.destroy()
+    answer_buttons_frame = tk.Frame(widget)
+    answer_buttons_frame.pack()
+    next_action()
 
-# widget.mainloop()
+def request_answer(question_fact: dict[str, str | tuple]) -> None:
+    options = (question_fact[key] for key in question_fact if key.startswith("answer") and question_fact[key])
+    question_var.set(question_fact["display"])
+
+    def answer_command(arg: str):
+        return lambda: answer_question(arg)
+    
+    for option in options:
+        button = tk.Button(answer_buttons_frame, text=option[0], command=answer_command(option[1]))
+        button.pack()
+
+next_action()
+widget.mainloop()
